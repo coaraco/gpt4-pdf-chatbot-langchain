4b9f96d19ce75a8a4aa9736d762d9faac47d8547
Ramon Cuni <rcuni8@gmail.com>
2023-04-19 15:51:38 +0200

    read pdf page by page

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,4 +1,3 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings';
 import { PineconeStore } from 'langchain/vectorstores';
 import { pinecone } from '@/utils/pinecone-client';
@@ -19,22 +18,13 @@ export const run = async () => {
     // const loader = new PDFLoader(filePath);
     const rawDocs = await directoryLoader.load();
 
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
-    });
-
-    const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
-
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
     const embeddings = new OpenAIEmbeddings();
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
     //embed the PDF documents
-    await PineconeStore.fromDocuments(docs, embeddings, {
+    await PineconeStore.fromDocuments(rawDocs, embeddings, {
       pineconeIndex: index,
       namespace: PINECONE_NAME_SPACE,
       textKey: 'text',
