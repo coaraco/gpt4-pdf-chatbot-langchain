0202f1aa5776ac245a6b0ca6947956e5e18d2094
Miquel Ferrer Llompart <hola@miquelferrerllompart.com>
2023-04-20 11:19:55 +0200

    - Add splitter by chunks

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,3 +1,4 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings';
 import { PineconeStore } from 'langchain/vectorstores';
 import { pinecone } from '@/utils/pinecone-client';
@@ -17,13 +18,20 @@ export const run = async () => {
 
     const rawDocs = await directoryLoader.load();
 
+     /* Split text into chunks */
+     const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+    const docs = await textSplitter.splitDocuments(rawDocs);
+
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
     const embeddings = new OpenAIEmbeddings();
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
     //embed the PDF documents
-    await PineconeStore.fromDocuments(rawDocs, embeddings, {
+    await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
       namespace: PINECONE_NAME_SPACE,
       textKey: 'text',
